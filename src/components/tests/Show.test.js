a4942cd2c98e2576c53a81ea2358cc2f82eb28bb
Nicholas Smith <nicholassmithbus@gmail.com>
2021-03-22 16:18:43 -0600

    completed MVP

--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -1,26 +1,86 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import React from "react";
+import { getByAltText, render, screen, within } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-import Show from './../Show';
+import Show from "./../Show";
 
 const testShow = {
-    //add in approprate test data structure here.
-}
+  name: "Stranger Things",
+  summary:
+    "A love letter to the '80s classics that captivated a generation, Stranger Things is set in 1983 Indiana, where a young boy vanishes into thin air. As friends, family and local police search for answers, they are drawn into an extraordinary mystery involving top-secret government experiments, terrifying supernatural forces and one very strange little girl.",
+  seasons: [
+    {
+      id: 0,
+      name: "Season 1",
+      episodes: [
+        {
+          id: 1,
+          name: "",
+          image: null,
+          season: 1,
+          number: 1,
+          summary: "This is a specific summary",
+          runtime: 1,
+        },
+      ],
+    },
+    {
+      id: 1,
+      name: "Season 2",
+      episodes: [],
+    },
+    {
+      id: 2,
+      name: "Season 3",
+      episodes: [],
+    },
+    {
+      id: 3,
+      name: "Season 4",
+      episodes: [],
+    },
+  ],
+};
 
-test('renders testShow and no selected Season without errors', ()=>{
+test("renders testShow and no selected Season without errors", () => {
+  render(<Show show={testShow} selectedSeason='none' />);
 });
 
-test('renders Loading component when prop show is null', () => {
+test("renders Loading component when prop show is null", () => {
+  render(<Show show={null} selectedSeason='none' />);
+  const loadingComponent = screen.queryByText("Fetching data...");
+  expect(loadingComponent).toBeInTheDocument();
 });
 
-test('renders same number of options seasons are passed in', ()=>{
+test("renders same number of options seasons are passed in", () => {
+  render(<Show show={testShow} selectedSeason='none' />);
+  const selects = screen.queryAllByTestId("season-option");
+  expect(selects).toHaveLength(4);
 });
 
-test('handleSelect is called when an season is selected', () => {
+test("handleSelect is called when an season is selected", () => {
+  const mockHandleSelect = jest.fn();
+
+  render(
+    <Show
+      show={testShow}
+      selectedSeason='none'
+      handleSelect={mockHandleSelect}
+    />
+  );
+  const select = screen.queryByLabelText("Select A Season");
+  userEvent.selectOptions(select, ["2"]);
+  expect(mockHandleSelect.mock.calls).toHaveLength(1);
 });
 
-test('component renders when no seasons are selected and when rerenders with a season passed in', () => {
+test("component renders when no seasons are selected and when rerenders with a season passed in", async () => {
+  const show = render(<Show show={testShow} selectedSeason='none' />);
+  const episodeNumber = screen.queryByText("Season 2, Episode 1");
+  expect(episodeNumber).not.toBeInTheDocument();
+
+  show.rerender(<Show show={testShow} selectedSeason='0' />);
+  const newEpisodeNumber = screen.queryByText("Season 1, Episode 1");
+  expect(newEpisodeNumber).toBeInTheDocument();
 });
 
 //Tasks:
